f2d9789eaac65b8044e4f2955b872a1aaa494187
Akash Kothawale <akash@decached.com>
2018-09-11 14:22:59 -0500

    constants: go fmt

--- constants.go
+++ constants.go
@@ -1,17 +1,28 @@
 package kinto
 
 const (
-	INFO_URI        = "/"
-	BATCH_URI       = "/batch"
-	HEARTBEAT_URI   = "/__heartbeat__"
-	FLUSH_URI       = "/__flush__"
-	OPENAPI_URI     = "/__api__"
-	BUCKETS_URI     = "/buckets"
-	BUCKET_URI      = "/buckets/%s"
-	GROUPS_URI      = "/buckets/%s/groups"
-	GROUP_URI       = "/buckets/%s/groups/%s"
+	INFO_URI  = "/"
+	BATCH_URI = "/batch"
+
+	HEARTBEAT_URI    = "/__heartbeat__"
+	LB_HEARTBEAT_URI = "/__lbheartbeat__"
+
+	OPENAPI_URI = "/__api__"
+	VERSION_URI = "/__version__"
+
+	FLUSH_URI = "/__flush__"
+
+	BUCKETS_URI = "/buckets"
+	BUCKET_URI  = "/buckets/%s"
+
 	COLLECTIONS_URI = "/buckets/%s/collections"
 	COLLECTION_URI  = "/buckets/%s/collections/%s"
-	RECORDS_URI     = "/buckets/%s/collections/%s/records"
-	RECORD_URI      = "/buckets/%s/collections/%s/records/%s"
+
+	RECORDS_URI = "/buckets/%s/collections/%s/records"
+	RECORD_URI  = "/buckets/%s/collections/%s/records/%s"
+
+	GROUPS_URI = "/buckets/%s/groups"
+	GROUP_URI  = "/buckets/%s/groups/%s"
+
+	ACCOUNT_URI = "/accounts/%s"
 )
